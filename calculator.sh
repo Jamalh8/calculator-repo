badbeb2244b325841aadd6124cebc931e19dd13f
Jamalh8 <jamalh.888@gmail.com>
2022-01-21 14:54:16 +0000

    EX-13 #in-progress updated calc

--- calculator.sh
+++ calculator.sh
@@ -1,29 +1,13 @@
 #!/bin/bash
 
-# Take user input
-echo "Please enter in two numbers : "
-read a
-read b
-
-# Input type of operation
-
-echo "Enter what you'd like to do :"
-echo "1. Addition"
-echo "2. Subtract"
-echo "3. Multiply"
-echo "4. Divide"
-read ch
-
-
-
 # Swtich Case to perform
 # calculator operations
-case $ch in
-1)res=`echo $(($a + $b))`;;
-2)res=`echo $(($a - $b))`;;
-3)res=`echo $(($a * $b))`;;
-4)res=`echo $(($a / $b))`;;
+case $3 in
+1)res=`echo $(($1 + $2))`;;
+2)res=`echo $(($1 - $2))`;;
+3)res=`echo $(($1 * $2))`;;
+4)res=`echo $(($1 / $2))`;;
 esac
 
-echo "Result : $res"
+echo $res
 

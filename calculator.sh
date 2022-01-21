94771143df95e950451bd716568270b3b9473117
Jamalh8 <jamalh.888@gmail.com>
2022-01-21 13:42:37 +0000

    EX-12 #done Calculator created

--- calculator.sh
+++ calculator.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 
-echo "This is my test for a calcultor script"
+# Take user input
+echo "Please enter in two numbers : "
+read a
+read b
+
+# Input type of operation
+
+echo "Enter what you'd like to do :"
+echo "1. Addition"
+echo "2. Subtract"
+echo "3. Multiply"
+echo "4. Divide"
+read ch
+
+
+
+# Swtich Case to perform
+# calculator operations
+case $ch in
+1)res=`echo $(($a + $b))`;;
+2)res=`echo $(($a - $b))`;;
+3)res=`echo $(($a * $b))`;;
+4)res=`echo $(($a / $b))`;;
+esac
+
+echo "Result : $res"
 
